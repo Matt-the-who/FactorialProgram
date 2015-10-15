4b8c02d244231422b8dbfef447d3c99ca48c07c2
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-10-15 12:52:17 -0400

    Factorial Program using For loops

---
+++ FactorialProgram/Factorial.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FactorialProgram
+{
+    partial class frmFactorialNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFactorialNumbers = new System.Windows.Forms.ListBox();
+            this.txtStartNumber = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstFactorialNumbers
+            // 
+            this.lstFactorialNumbers.FormattingEnabled = true;
+            this.lstFactorialNumbers.Location = new System.Drawing.Point(59, 76);
+            this.lstFactorialNumbers.Name = "lstFactorialNumbers";
+            this.lstFactorialNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstFactorialNumbers.TabIndex = 0;
+            // 
+            // txtStartNumber
+            // 
+            this.txtStartNumber.Location = new System.Drawing.Point(232, 32);
+            this.txtStartNumber.Name = "txtStartNumber";
+            this.txtStartNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtStartNumber.TabIndex = 1;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(28, 35);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(198, 13);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Enter an integer to calculate the factorial";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(244, 58);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(209, 158);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 4;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(424, 24);
+            this.mnuStrip.TabIndex = 5;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmFactorialNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtStartNumber);
+            this.Controls.Add(this.lstFactorialNumbers);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmFactorialNumbers";
+            this.Text = "Factorial Program";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFactorialNumbers;
+        private System.Windows.Forms.TextBox txtStartNumber;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ FactorialProgram/Factorial.cs
@@ -0,0 +1,65 @@
+﻿/*
+Created by Matthew Ho
+Created on 06-10-15
+Created for ICS3U
+Daily task 3-05
+This program calculates factorials `using repeats.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialProgram
+{
+    public partial class frmFactorialNumbers : Form
+    {
+        public frmFactorialNumbers()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Variables
+            Double factorialNumber;
+            Double factorialAnswer;
+
+
+            
+            this.lstFactorialNumbers.Items.Clear();
+            factorialAnswer = 1;
+            factorialNumber = Convert.ToDouble(this.txtStartNumber.Text);
+
+
+            //process
+            /*
+            do
+            {
+                factorialCounter = factorialCounter + 1;
+                this.lstFactorialNumbers.Items.Add(factorialCounter);
+                factorialAnswer = factorialAnswer * factorialCounter;
+            } while (factorialNumber != factorialCounter);
+            */
+
+            for (int factorialCounter = 1; factorialCounter<= factorialNumber; factorialCounter++)
+            {
+                this.lstFactorialNumbers.Items.Add(factorialCounter);
+                factorialAnswer = factorialAnswer * factorialCounter;
+            }
+
+            this.lblAnswer.Text = Convert.ToString(factorialAnswer);
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
